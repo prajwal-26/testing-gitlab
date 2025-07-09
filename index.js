ac6a8592de62f21789b38e1e18788f0d51a34a9f
prajwal patil <127192880+prajwal-26@users.noreply.github.com>
2025-07-09 13:54:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 console.log("Node.js test app: Hello from GitHub testing!");
 console.log("successfully tested the webhook!");
 console.log("test")
+console.log("working")
+
 // testing the webhooks api
